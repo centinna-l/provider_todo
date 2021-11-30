90b0d58542a516791a57c647de3f41b1a6d7a100
centinna-l <jerryjoy99@gmail.com>
2021-11-30 14:49:32 +0530

    Todo Flutter Provider

---
+++ lib/item_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_state_management/item_list_provider.dart';
+import 'package:flutter_state_management/todo_model.dart';
+import 'package:provider/provider.dart';
+
+class ItemView extends StatefulWidget {
+  final Todo item;
+
+  ItemView({this.item});
+
+  @override
+  _ItemViewState createState() => _ItemViewState();
+}
+
+class _ItemViewState extends State<ItemView> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    if (widget.item != null)
+      _textEditingController.text = widget.item.description;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('New task'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: _textEditingController,
+                onFieldSubmitted: (value) => submit(),
+                textCapitalization: TextCapitalization.sentences,
+                autofocus: true,
+              ),
+              SizedBox(height: 10),
+              // ignore: deprecated_member_use
+              RaisedButton(
+                child: Text('Submit'),
+                onPressed: submit,
+              )
+            ],
+          ),
+        ));
+  }
+
+  void submit() {
+    String description = _textEditingController.text;
+    if (description != null && description.isNotEmpty) {
+      if (widget.item != null) {
+        context.read<StateProvider>().editTask(widget.item, description);
+      } else {
+        context.read<StateProvider>().addNewTask(description);
+      }
+      Navigator.pop(context, _textEditingController.text);
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_state_management/item_list_provider.dart';
+import 'package:flutter_state_management/item_view.dart';
+import 'package:flutter_state_management/todo_model.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider(
+    create: (context) => StateProvider(),
+    child: MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo App'),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: goToNewItemView,
+      ),
+      body: Consumer<StateProvider>(builder: (context, stateProvider, child) {
+        return stateProvider.items.isNotEmpty
+            ? ListView.builder(
+                itemCount: stateProvider.items.length,
+                itemBuilder: (context, index) {
+                  return TodoItem(
+                    item: stateProvider.items[index],
+                    onTap: stateProvider.chanceCompleteness,
+                    onLongPress: goToEditItemView,
+                    onDismissed: stateProvider.removeItem,
+                  );
+                },
+              )
+            : Center(
+                child: Text('No items'),
+              );
+      }),
+    );
+  }
+
+  // Navigation
+
+  void goToNewItemView() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return ItemView();
+    }));
+  }
+
+  void goToEditItemView(Todo item) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      return ItemView(item: item);
+    }));
+  }
+}
+
+class TodoItem extends StatelessWidget {
+  final Todo item;
+  final Function(Todo) onTap;
+  final Function(Todo) onLongPress;
+  final Function(Todo) onDismissed;
+
+  TodoItem(
+      {@required this.item,
+      @required this.onTap,
+      @required this.onDismissed,
+      @required this.onLongPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: Key(item.hashCode.toString()),
+      direction: DismissDirection.startToEnd,
+      background: Container(
+        padding: EdgeInsets.only(left: 12),
+        color: Colors.red,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+        ),
+        alignment: Alignment.centerLeft,
+      ),
+      onDismissed: (direction) => onDismissed(item),
+      child: ListTile(
+        title: Text(
+          item.description,
+          style: TextStyle(
+              decoration: item.complete ? TextDecoration.lineThrough : null),
+        ),
+        trailing: Icon(
+            item.complete ? Icons.check_box : Icons.check_box_outline_blank),
+        onTap: () => onTap(item),
+        onLongPress: () => onLongPress(item),
+      ),
+    );
+  }
+}
